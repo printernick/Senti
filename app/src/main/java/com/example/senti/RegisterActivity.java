190d11740b76482d7f84cde3a9c667de6e464fe1
Nicholas Tran <phikhant@uci.edu>
2019-04-20 23:09:22 -0700

    Updated register activity to register users.

--- app/src/main/java/com/example/senti/RegisterActivity.java
+++ app/src/main/java/com/example/senti/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.example.senti;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,9 +15,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private EditText nameField;
     private EditText emailField;
     private EditText passwordField;
 
@@ -27,8 +30,10 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        nameField = findViewById(R.id.fieldName);
         emailField = findViewById(R.id.fieldEmail);
         passwordField = findViewById(R.id.fieldPassword);
+
 
         findViewById(R.id.signUp).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,16 +62,21 @@ public class RegisterActivity extends AppCompatActivity {
                         {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d("login", "createUserWithEmail:success");
+
                             FirebaseUser user = mAuth.getCurrentUser();
-                            //updateUI(user);
+                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                    .setDisplayName(nameField.getText().toString()).build();
+                            user.updateProfile(profileUpdates);
+                            updateUI(user);
                         }
+
                         else
                         {
                             // If sign in fails, display a message to the user.
                             Log.w("login", "createUserWithEmail:failure", task.getException());
                             Toast.makeText(RegisterActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
-                            //updateUI(null);
+                            updateUI(null);
                         }
                     }
                 });
@@ -75,6 +85,17 @@ public class RegisterActivity extends AppCompatActivity {
     private boolean validateForm()
     {
         boolean valid = true;
+
+        String name = nameField.getText().toString();
+        if (TextUtils.isEmpty(name))
+        {
+            nameField.setError("Required.");
+            valid = false;
+        }
+        else
+        {
+            nameField.setError(null);
+        }
 
         String email = emailField.getText().toString();
         if (TextUtils.isEmpty(email))
@@ -98,5 +119,14 @@ public class RegisterActivity extends AppCompatActivity {
         }
 
         return valid;
+    }
+
+    private void updateUI(FirebaseUser user)
+    {
+        if (user != null)
+        {
+            startActivity(new Intent(RegisterActivity.this, CalendarActivity.class));
+        }
+
     }
 }
